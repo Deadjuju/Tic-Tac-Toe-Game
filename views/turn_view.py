c5fac352982f479712f6961fda43b3928a76bdd3
Julien Garcia <julien.garcia-lp@laposte.net>
2022-07-21 11:37:35 +0200

    feat(views): init views and add turn_view

---
+++ views/turn_view.py
@@ -0,0 +1,19 @@
+
+class TurnView:
+    
+    
+    @classmethod
+    def announce_the_player(cls, player):
+        print("_" * 20)
+        print(f"{player.username} plays! ")
+        print(f"Pointer: {player.pointer} ")
+        print("-" * 20)
+
+    @classmethod
+    def prompt_confirmation(cls, box, grid):
+        grid.display_current_grid()
+        choice_confirmation = input(f"Your choice: {box}.\n Type - no - to choose an other box.\n-> ")
+        if choice_confirmation == "no" or choice_confirmation == "NO":
+            return False
+        return True
+    
