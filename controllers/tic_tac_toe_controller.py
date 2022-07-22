cea4285efdbdcaccd83a2edd1be832411d54e655
Julien Garcia <julien.garcia-lp@laposte.net>
2022-07-22 10:02:10 +0200

    refactor(TicTacToe): refactor main controller
    add docstrings refactor and transfere all prints in view

--- controllers/tic_tac_toe_controller.py
+++ controllers/tic_tac_toe_controller.py
@@ -10,6 +10,9 @@ VICTORY_POINTS: int = 2
 
 
 class TicTacToe:
+    """
+    class grouping all the parts composing the game    
+    """
 
     def __init__(self, grid: Grid, view: TicTacToeView, player_manager: Player) -> None:
         self.grid = grid
@@ -17,27 +20,54 @@ class TicTacToe:
         self.player_manager = player_manager
 
     def _begin_or_quit_the_game(self) -> str:
-        return self.view.begin_or_quit()
+        """asks players if they want to start a game
+
+        Returns:
+            str: quit or begin
+        """
+
+        begin_or_quit = self.view.begin_or_quit()
+        if begin_or_quit.lower() == "q" or begin_or_quit.lower() == "quit":
+            return "quit"
+        self.view.game_begin_message()
+        return "begin"
 
     def _choose_who_starts(self, player1: Player, player2: Player) -> Player:
+        """random choice of the player who starts + waiting message
+
+        Args:
+            player1 (Player): 
+            player2 (Player): 
+
+        Returns:
+            Player: starting player
+        """
+        
+        self.view.waiting_for_starter_player()
         starter = choice((player1, player2))
         self.view.display_of_the_starting_player(starter)
         return starter
 
-    def _check_if_username_is_free(self, username1, pointer2) -> str:
+    def _check_if_username_is_free_and_choose_it(self, username1: str, pointer2: Pointer) -> str:
+        """ check the availability of the username """
+
         username2 = self._info_choose_your_username(player_number=2, pointer_obj=pointer2)
         if username1 == username2:
-            print("This username is not free")
-            return self._check_if_username_is_free(username1, pointer2)
+            self.view.username_already_used()
+            return self._check_if_username_is_free_and_choose_it(username1, pointer2)
         return username2
 
     @classmethod
     def _change_player(cls, current_player: Player, player1: Player, player2: Player) -> Player: 
+        """ player switch """
+
         if current_player == player1:
             return player2
         return player1
 
     def _info_choose_your_username(self, player_number: int, pointer_obj: Pointer) -> str:
+        """ prompt the player to choose an username """
+
         pointer = pointer_obj.value
         username = self.view.prompt_for_username(player_number, pointer)
         if username == "":
@@ -45,9 +75,13 @@ class TicTacToe:
             return self._info_choose_your_username(player_number, pointer_obj)
         return username        
 
-    def run(self):
+    def run(self) -> None:
+        """
+        Course of a game
+        """
+
         # Welcome part
-        self.view.welcome()
+        self.view.welcome_message()
 
         begin = self._begin_or_quit_the_game()
 
@@ -63,7 +97,7 @@ class TicTacToe:
 
         # choose player 2
         pointer2 = Pointer.CROSS
-        username2 = self._check_if_username_is_free(username1, pointer2)
+        username2 = self._check_if_username_is_free_and_choose_it(username1, pointer2)
         player2 = Player(username2, pointer2)
 
         while is_game_runing:
@@ -94,8 +128,8 @@ class TicTacToe:
                 if is_player_winner:
                     # we have a winner
                     current_player.score += VICTORY_POINTS
-                    print(f"{current_player.username} WIN !!!")
-                    input()
+                    winner = current_player.username
+                    self.view.congrat_the_winner(winner)
 
                     # Play an other part?
                     self.view.show_score(player1, player2)
@@ -106,7 +140,7 @@ class TicTacToe:
                     #  draw situation
                     player1.score += DRAW_POINTS
                     player2.score += DRAW_POINTS
-                    print("It's a draw!!!")
+                    self.view.announce_equality()
 
                     self.view.show_score(player1, player2)
                     is_game_over = True
--- views/menu_view.py
+++ views/menu_view.py
@@ -10,12 +10,62 @@ TICTACTOE = """
       \/ |    | /\ 
       /\ |    | \/ 
 """
-
+POURCENT_1 = "▒▒▒▒▒▒▒▒▒▒ 0%"
+POURCENT_2 = "█▒▒▒▒▒▒▒▒▒ 10%"
+POURCENT_3 = "██▒▒▒▒▒▒▒▒ 20%"
+POURCENT_4 = "███▒▒▒▒▒▒▒ 30%"
+POURCENT_5 = "████▒▒▒▒▒▒ 40%"
+POURCENT_6 = "█████▒▒▒▒▒ 50%"
+POURCENT_7 = "██████▒▒▒▒ 60%"
+POURCENT_8 = "███████▒▒▒ 70%"
+POURCENT_9 = "████████▒▒ 80%"
+POURCENT_10 = "█████████▒ 90%"
+POURCENT_11 = "██████████ 100%"
+FIREWORK = """
+         .* *.               `o`o`
+         *. .*              o`o`o`o      ^,^,^
+           * \               `o`o`     ^,^,^,^,^
+              \     ***        |       ^,^,^,^,^
+               \   *****       |        /^,^,^
+                \   ***        |       /
+    ~@~*~@~      \   \         |      /
+  ~*~@~*~@~*~     \   \        |     /
+  ~*~@smd@~*~      \   \       |    /     #$#$#        .`'.;.
+  ~*~@~*~@~*~       \   \      |   /     #$#$#$#   00  .`,.',
+    ~@~*~@~ \        \   \     |  /      /#$#$#   /|||  `.,'
+_____________\________\___\____|_/______/_________|\/\___||______
+"""
 
 class TicTacToeView:
+
+    @classmethod
+    def _print_loader(cls, time_transition: float) -> None:
+        print(POURCENT_1)
+        sleep(time_transition)
+        print(POURCENT_2)
+        sleep(time_transition)
+        print(POURCENT_3)
+        sleep(time_transition)
+        print(POURCENT_4)
+        sleep(time_transition)
+        print(POURCENT_5)
+        sleep(time_transition)
+        print(POURCENT_6)
+        sleep(time_transition)
+        print(POURCENT_7)
+        sleep(time_transition)
+        print(POURCENT_8)
+        sleep(time_transition)
+        print(POURCENT_9)
+        sleep(time_transition)
+        print(POURCENT_10)
+        sleep(time_transition)
+        print(POURCENT_11)
+        sleep(time_transition)
+
     
     @classmethod
-    def welcome(cls) -> None:
+    def welcome_message(cls) -> None:
         print("--- WELCOME TO THE TIC-TAC-TOE GAME!--- ")
         print(TICTACTOE)
         sleep(1)
@@ -25,21 +75,34 @@ class TicTacToeView:
     def begin_or_quit(cls) -> str:
         message = "To quit the game type - q - or - quit -\nElse type anything. -> "
         begin_or_quit = input(message)
-        if begin_or_quit == "q" or begin_or_quit == "quit":
-            return "quit"
-        print("Let the game begin")
+        return begin_or_quit
+
+    @classmethod
+    def game_begin_message(cls) -> None:
+        print("/" * 25)
+        sleep(0.8)
+        print("Let the game begin\n")
         sleep(1)
-        return "begin"
+
+    @classmethod
+    def waiting_for_starter_player(cls) -> None:
+        """ Display a waiting message with a false loader """
+        print("Please wait, We draw lots the player who will start")
+        sleep(1)
+        TicTacToeView._print_loader(0.06)
+
 
     @classmethod
     def display_of_the_starting_player(cls, player: Player) -> None:
         print(f"{player.username} starts the game")
         sleep(0.75)
-        print("-" * 20)
+        print(f'{"-" * 20}\n')
         sleep(1)
 
     @classmethod
     def show_score(cls, player1: Player, player2: Player) -> None:
+        print("\n -----__________ SCORE: __________-----")
+        sleep(0.1)
         print(f"{player1.username}|score: {player1.score} - VS - {player2.username}|score: {player2.score}")
         sleep(2)
 
@@ -59,6 +122,33 @@ class TicTacToeView:
         print("/!\ WARNING /!\\")
         sleep(1)
         print("This field can not be empty.")
+        sleep(1)
+
+    @classmethod
+    def username_already_used(cls) -> None:
+        print("\n---------- INFORMATION ----------")
+        print("-" * 33)
+        sleep(1)
+        print("This username is already in use")
+        sleep(0.5)
+        print("Please choose an other one\n")
+        sleep(1)
+        
+    @classmethod
+    def congrat_the_winner(cls, winner: str) -> None:
+        print("\nWe've got a winner!!!")
+        sleep(0.8)
+        print(f"{winner} WIN !!!")
+        sleep(1.5)
+        print(FIREWORK)
+        sleep(0.1)
+        print()
+        sleep(1.5)
+
+    @classmethod
+    def announce_equality(cls) -> None:
+        print("\n ----- RESULT -----")
+        print("It's a draw!!! \n")
         sleep(1)
 
     @classmethod
